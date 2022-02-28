deeb92d1148eb84fdca580b96e6a29b61ea00ffb
Luca Calcaterra <calcaterra.luca@gmail.com>
2022-02-28 19:32:43 +0100

    fix

--- database/migrations/2022_02_24_201055_create_uffici_table.php
+++ database/migrations/2022_02_24_201055_create_uffici_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('uffici', function (Blueprint $table) {
             $table->id();
             $table->string('nome');
-            $table->$table->timestamps();
+            $table->timestamps();
         });
     }
 
