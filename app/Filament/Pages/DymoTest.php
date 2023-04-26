d552b9657586074c1c38703e3b65baf49fa9eed3
Luca Calcaterra <calcaterra.luca@gmail.com>
2023-04-26 19:37:23 +0200

    dymo connect test

---
+++ app/Filament/Pages/DymoTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class DymoTest extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.dymo-test';
+
+
+}
