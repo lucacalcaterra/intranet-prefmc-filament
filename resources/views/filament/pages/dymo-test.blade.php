c77e1f8f555645f0706872d275dc06416b9553e0
Luca Calcaterra <calcaterra.luca@gmail.com>
2023-04-26 19:39:06 +0200

    fix

--- resources/views/filament/pages/dymo-test.blade.php
+++ resources/views/filament/pages/dymo-test.blade.php
@@ -3,15 +3,14 @@
 </x-filament::page>
 
 
-{{--
+
 <script>
-    src="https://github.com/dymosoftware/dymo-connect-framework/raw/master/dymo.connect.framework.js"
 
 
     document.addEventListener('livewire:load', function () {
         console.log("ciao")
-        @this.dymo.label.framework.init() //Initialize DYMO Label Framework
+        dymo.label.framework.init() //Initialize DYMO Label Framework
     })
 
     // "posts" will now be a JavaScript array of post data from PHP.
-</script> --}}
+</script>
